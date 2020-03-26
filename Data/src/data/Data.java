ec0dbeec1f619339fc5306eabdbc435ed34ecdb7
Ra Ven <laingraven.java@gmail.com>
2020-03-26 22:49:08 +0700

    first commit

---
+++ Data/src/data/Data.java
@@ -0,0 +1,49 @@
+package data;
+
+import java.io.Serializable;
+import javax.swing.ImageIcon;
+
+public class Data implements Serializable {
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public ImageIcon getImage() {
+        return image;
+    }
+
+    public void setImage(ImageIcon image) {
+        this.image = image;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public byte[] getFile() {
+        return file;
+    }
+
+    public void setFile(byte[] file) {
+        this.file = file;
+    }
+
+    private String status;
+    private ImageIcon image;
+    private byte[] file;
+    private String name;
+}
